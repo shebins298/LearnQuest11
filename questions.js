0622c2e1be585bb4bd273b3ce19e55e79a2784c7
shebins298 <shebins298@gmail.com>
2025-07-02 15:07:54 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,154 @@
+const cppQuestions = {
+  "Chapter 1 - Fundamentals of Computer": [
+    {
+      question: "Meaningful and processed form of data is known as",
+      options: ["Data", "Program", "Instruction", "Information"],
+      answer: "Information",
+    },
+    {
+      question: "Which of the following is known as the brain of the computer?",
+      options: [
+        "Central Processing Unit",
+        "Control Unit",
+        "Arithmetic Logic Unit",
+        "Monitor",
+      ],
+      answer: "Central Processing Unit",
+    },
+    {
+      question:
+        "CPU has three components. Which one of the following is the CORRECT option?",
+      options: [
+        "ALU, CU and ROM",
+        "ALU, CU and RAM",
+        "ALU, CU and Registers",
+        "ALU, RAM and ROM",
+      ],
+      answer: "ALU, CU and Registers",
+    },
+    {
+      question:
+        "Name the character representation coding scheme developed in India and approved by the Bureau of Indian Standards (BIS).",
+      options: ["ASCII", "ISCII", "EBCDIC", "UTF-8"],
+      answer: "ISCII",
+    },
+    {
+      question: "Write the full form of ASCII.",
+      options: [
+        "American Standard Code for Inter Communication",
+        "Automatic Standard Code for Information Interchange",
+        "American Standard Code for Information Interchange",
+        "Advanced Standard Code for International Communication",
+      ],
+      answer: "American Standard Code for Information Interchange",
+    },
+    {
+      question: "Write the full form of JPEG.",
+      options: [
+        "Joint Picture Encoding Group",
+        "Joint Photographic Experts Group",
+        "Joint Picture Experts Group",
+        "Joint Pixel Encoding Group",
+      ],
+      answer: "Joint Picture Experts Group",
+    },
+  ],
+  "Chapter 2 - Components of the Computer System": [
+    {
+      question:
+        "The register in CPU that holds address of next instruction to be executed.",
+      options: [
+        "Memory Address Register",
+        "Program Counter (PC)",
+        "Instruction Register",
+        "Accumulator",
+      ],
+      answer: "Program Counter (PC)",
+    },
+    {
+      question: "USB stands for ...........",
+      options: [
+        "United System Bus",
+        "Universal Standard Bus",
+        "Universal Serial Bus",
+        "Unified Serial Bus",
+      ],
+      answer: "Universal Serial Bus",
+    },
+    {
+      question: "Write the full form of HDMI.",
+      options: [
+        "High Definition Multimedia Interface",
+        "High Digital Multimedia Interface",
+        "High Dynamic Media Interface",
+        "High Definition Media Integration",
+      ],
+      answer: "High Definition Multimedia Interface",
+    },
+    {
+      question:
+        "______ memory is a small and fast memory between the processor and RAM.",
+      options: ["Virtual", "Main", "Cache", "Flash"],
+      answer: "Cache",
+    },
+    {
+      question:
+        "Mr. Rajmohan wants to buy a computer. He is an engineer by profession. He wants a device which can be used to ‘draw directly on screen’. Suggest him an input device.",
+      options: ["Joystick", "Mouse", "Light Pen", "Trackball"],
+      answer: "Light Pen",
+    },
+    {
+      question: "What is the full form of MICR?",
+      options: [
+        "Machine Input Code Reader",
+        "Magnetically Interpreted Code Reader",
+        "Magnetic Input Character Recognition",
+        "Magnetic Ink Character Recognition",
+      ],
+      answer: "Magnetic Ink Character Recognition",
+    },
+    {
+      question: "Which printer is widely used as portable printer?",
+      options: [
+        "Inkjet Printer",
+        "Dot Matrix Printer",
+        "Thermal Printer",
+        "Laser Printer",
+      ],
+      answer: "Thermal Printer",
+    },
+    {
+      question:
+        "Write an example of an operating system that is a free and open source software.",
+      options: ["Windows", "macOS", "UNIX", "GNU/Linux"],
+      answer: "GNU/Linux",
+    },
+    {
+      question:
+        "C++ uses the .................. language processor for translation.",
+      options: ["Compiler", "Interpreter", "Assembler", "Loader"],
+      answer: "Compiler",
+    },
+    {
+      question:
+        "Name the software that translates assembly language program into machine language.",
+      options: ["Compiler", "Linker", "Assembler", "Interpreter"],
+      answer: "Assembler",
+    },
+    {
+      question: "Program written in HLL is known as",
+      options: ["Byte Code", "Object Code", "Executable Code", "Source Code"],
+      answer: "Source Code",
+    },
+    {
+      question: "What is the full form of FSF?",
+      options: [
+        "Free Source Federation",
+        "Free Software Foundation",
+        "File Storage Format",
+        "Foundation of Software Freedom",
+      ],
+      answer: "Free Software Foundation",
+    },
+  ],
+};
